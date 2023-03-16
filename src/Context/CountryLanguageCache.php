8f4cf3ce6e0ea5676d25a087b98f4ded3284ee02
Borja Vicente <nireneko@gmail.com>
2023-03-16 22:41:06 +0100

    Added comments to the cache context class.

--- src/Context/CountryLanguageCache.php
+++ src/Context/CountryLanguageCache.php
@@ -8,10 +8,26 @@ use Drupal\Core\Cache\Context\RequestStackCacheContextBase;
 use Drupal\country_language_url\Service\CountryInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 
+/**
+ * This class defines a cache context for country.
+ */
 class CountryLanguageCache extends RequestStackCacheContextBase implements CacheContextInterface {
 
+  /**
+   * The service country_language_url.country.
+   *
+   * @var \Drupal\country_language_url\Service\CountryInterface
+   */
   protected CountryInterface $country;
 
+  /**
+   * Constructor of the class.
+   *
+   * @param \Symfony\Component\HttpFoundation\RequestStack $request_stack
+   *   The service request_stack.
+   * @param \Drupal\country_language_url\Service\CountryInterface $country
+   *   The service country_language_url.country.
+   */
   public function __construct(RequestStack $request_stack, CountryInterface $country) {
     parent::__construct($request_stack);
     $this->country = $country;
